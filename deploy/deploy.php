8545aad89bc18fc33146a270c952417eca1e1412
Takahiro Nakamura <nakamura@jword.jp>
2016-09-13 20:15:46 +0900

    test commit

---
+++ deploy/deploy.php
@@ -0,0 +1,14 @@
+<?php
+
+// デプロイ先のサーバを定義します。
+server('test.server', '157.7.52.90 ', 22)
+    ->user('nakamura')
+    // ssh agentを使い認証します。
+    ->identityFile()
+    ->forwardAgent()
+    ->stage('production')
+    // デプロイ先のベースパスを定義します。
+    ->env('deploy_path', '/var/www/your/project');
+
+// Gitリポジトリを設定します。サーバ上にgitが必要です。
+set('repository', 'https://github.com/jwd-nakamura-t/testing.git');
