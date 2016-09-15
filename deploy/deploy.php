e9c80c8c6e2487ad81ace0eb0559fce149c4b466
Takahiro Nakamura <nakamura@jword.jp>
2016-09-15 17:08:36 +0900

    15

--- deploy/deploy.php
+++ deploy/deploy.php
@@ -3,7 +3,7 @@ require 'recipe/common.php';
 require 'recipe/symfony.php';
 
 // デプロイ先のサーバを定義します。
-server('test.server', '157.7.52.90 ', 22)
+server('test_server', '157.7.52.90 ', 22)
     ->user('nakamura')
     // ssh agentを使い認証します。
     ->identityFile()
@@ -12,6 +12,14 @@ server('test.server', '157.7.52.90 ', 22)
     // デプロイ先のベースパスを定義します。
     ->env('branch', 'develop')
     ->env('deploy_path', '/var/www/your/project');
+
+server('product_server', '157.7.52.90 ', 22)
+    ->user('nakamura')
+    ->identityFile()
+    ->forwardAgent()
+    ->stage('production')
+    ->env('branch', 'master')
+    ->env('deploy_path', '/var/www/html');
 
 // Gitリポジトリを設定します。サーバ上にgitが必要です。
 set('repository', 'https://github.com/jwd-nakamura-t/testing.git');
